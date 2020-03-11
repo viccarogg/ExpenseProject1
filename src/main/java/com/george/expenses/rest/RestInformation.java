164e160fba3fd0af68eb1c8690f8b8a3336200dd
viccarogg <viccarogg@gmail.com>
2020-03-10 20:35:21 -0400

    completed information REST

--- src/main/java/com/george/expenses/rest/RestInformation.java
+++ src/main/java/com/george/expenses/rest/RestInformation.java
@@ -1,5 +1,34 @@
 package com.george.expenses.rest;
 
-public class RestInformation {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import com.george.expenses.db.EmployeeInfoDAO;
+import com.george.expenses.db.EmployeeInfoDB;
+import com.george.expenses.models.EmployeeInfo;
+
+@Path("/info")
+public class RestInformation {
+	EmployeeInfoDAO infodb = new EmployeeInfoDB();
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public EmployeeInfo getInformation(@PathParam("id") int emp_id) {
+		EmployeeInfo info = infodb.getMyInformation(emp_id);
+		return info;
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateInformation(@PathParam("id") int emp_id, EmployeeInfo info) {
+		infodb.updateMyInformation(emp_id, info);
+	}
 }
